7f709782ae806360cdaae32ef4283f5e9e618f86
Andre Derjagin <derjagin.andre@gmail.com>
2023-11-29 14:35:23 +0100

    Add @radix-ui/react-toast and usehooks-ts dependencies

--- src/components/output/output.tsx
+++ src/components/output/output.tsx
@@ -1,20 +1,29 @@
 import { Button } from '@/components/ui/button';
 import { ClipboardCopy, Copy, Upload } from 'lucide-react';
 import React from 'react';
+import { useCopyToClipboard } from 'usehooks-ts';
 
 interface OutputProps {
 	response: string | null;
 }
 
 function Output({ response }: OutputProps) {
+	const [value, copy] = useCopyToClipboard();
+
+	const handleOnClick = () => {
+		copy(response || '');
+	};
+
 	return (
 		<div>
-			{/* {response && <div>{response}</div>} */}
 			<div className="relative border rounded-md p-4 max-h-96 overflow-y-auto">
 				<div className="">{response}</div>
 			</div>
 			<div className="flex justify-end">
-				<Button className="gap-2 mt-4 w-40">
+				<Button
+					className="gap-2 mt-4 w-40 z-20"
+					onClick={handleOnClick}
+				>
 					<Copy size={18} />
 					Copy Text
 				</Button>
