2400c0ce8cb09d10f4aa18573e94e65055a83397
Andre Derjagin <derjagin.andre@gmail.com>
2023-12-31 16:04:13 +0100

    updated output format

--- src/components/spech-to-text/speech-to-text.tsx
+++ src/components/spech-to-text/speech-to-text.tsx
@@ -1,20 +1,22 @@
-'use client';
+"use client";
 
-import AnimatedButton from '@/components/animated-button';
-import Output from '@/components/output';
-import ThreeDotsWave from '@/components/three-dots-wave';
-import { Input } from '@/components/ui/input';
-import axios from 'axios';
-import { Upload } from 'lucide-react';
-import React from 'react';
+import AnimatedButton from "@/components/animated-button";
+import Output from "@/components/output";
+import ThreeDotsWave from "@/components/three-dots-wave";
+import { Input } from "@/components/ui/input";
+import axios from "axios";
+import { Upload } from "lucide-react";
+import React from "react";
 
 const OPENAI_API_KEY = process.env.NEXT_PUBLIC_OPENAI_API_KEY;
-const model = 'whisper-1';
+const model = "whisper-1";
 
 // some small change
 function SpeechToText() {
 	const inputRef = React.useRef<HTMLInputElement>(null);
 	const [file, setFile] = React.useState<File | null>(null);
+	const [responseFormat, setResponseFormat] = React.useState<string>("srt");
+	// const [responseFormat, setResponseFormat] = React.useState<string>('text');
 	const [response, setResponse] = React.useState<string | null>(null);
 	const [isLoading, setIsLoading] = React.useState<boolean>(false);
 
@@ -28,19 +30,22 @@ function SpeechToText() {
 			if (!file) return;
 
 			const formData = new FormData();
-			formData.append('model', model);
-			formData.append('file', file);
+			formData.append("model", model);
+			formData.append("file", file);
+
+			if (responseFormat) formData.append("response_format", responseFormat);
 
 			axios
-				.post('https://api.openai.com/v1/audio/transcriptions', formData, {
+				.post("https://api.openai.com/v1/audio/transcriptions", formData, {
 					headers: {
-						'Content-Type': 'multipart/form-data',
+						"Content-Type": "multipart/form-data",
 						Authorization: `Bearer ${OPENAI_API_KEY}`,
 					},
 				})
 				.then((res) => {
-					setResponse(res.data.text);
+					setResponse(res.data);
 					setIsLoading(false);
+					// console.log('res', res);
 				})
 				.catch((err) => {
 					console.error(err);
@@ -48,13 +53,13 @@ function SpeechToText() {
 		};
 
 		fetchAudioFile();
-	}, [file]);
+	}, [file, responseFormat]);
 
 	return (
 		<div
 			className=""
 			style={{
-				backgroundColor: 'f2f2f2',
+				backgroundColor: "f2f2f2",
 				padding: 20,
 				borderRadius: 8,
 			}}
